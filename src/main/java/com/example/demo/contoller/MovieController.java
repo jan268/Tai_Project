e12108d62e9442d11ab479d42cc891df8e0df2a5
Jan <jan.witek88@gmail.com>
2021-01-13 11:36:54 +0100

    poprawki

--- src/main/java/com/example/demo/contoller/MovieController.java
+++ src/main/java/com/example/demo/contoller/MovieController.java
@@ -2,12 +2,14 @@ package com.example.demo.contoller;
 
 import com.example.demo.Services.MovieService;
 import com.example.demo.Services.ViewerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
+@Slf4j
 public class MovieController {
 
     private final ViewerService viewerService;
@@ -24,6 +26,23 @@ public class MovieController {
         model.addAttribute("movie", movieService.findByViewerIdAndMovieId(viewerId, movieId));
 
         return "viewer/movie/show";
+    }
+
+    @GetMapping("/viewer/{viewerId}/{movieId}/update")
+    public String updateMovie(@PathVariable Long viewerId, @PathVariable Long movieId, Model model){
+
+        model.addAttribute("movie", movieService.findByViewerIdAndMovieId(viewerId, movieId));
+
+        return "/viewer/movie/movieform";
+    }
+
+    @GetMapping("/viewer/{viewerId}/{movieId}/delete")
+    public String deleteMovie(@PathVariable Long viewerId, @PathVariable Long movieId){
+
+        log.debug("Deleted movie: " + movieId);
+        movieService.deleteById(viewerId, movieId);
+
+        return "redirect:/viewer/" + viewerId + "/show";
     }
 
 }
