bb77354c656c41b742e9144000bae443b58e8b45
Jan <jan.witek88@gmail.com>
2021-01-26 21:48:54 +0100

    New Validation(on ViewerCommand) - fixed a bug with updatev2 Works but doesnt give specific messages what is wrong

--- src/main/java/com/example/demo/Validation/ViewerValidation.java
+++ src/main/java/com/example/demo/Validation/ViewerValidation.java
@@ -3,12 +3,14 @@ package com.example.demo.Validation;
 import com.example.demo.Command.ViewerCommand;
 import com.example.demo.model.Viewer;
 import com.example.demo.repositories.ViewerRepository;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 public class ViewerValidation implements ConstraintValidator<UniqueViewer, ViewerCommand> {
 
     private final ViewerRepository viewerRepository;
@@ -25,8 +27,23 @@ public class ViewerValidation implements ConstraintValidator<UniqueViewer, Viewe
         Set<Viewer> viewerSet = new HashSet<>();
         viewerRepository.findAll().iterator().forEachRemaining(viewerSet::add);
 
-        if (viewerSet.stream().filter(workingViewer -> workingViewer.getId().equals(viewerCommand.getId())).findFirst().isPresent()){
-            return true;
+        if (viewerSet.stream().filter(workingViewer -> workingViewer.getId().equals(viewerCommand.getId())) //this line looks if there is such a record in DB
+                .findFirst().isPresent()){
+            if(viewerSet.stream().filter(viewer -> viewer.getUsername().equals(viewerCommand.getUsername())) //if there is Username in DB
+                    .findFirst().isPresent()) {
+                if (viewerSet.stream().filter(viewer -> viewer.getUsername().equals(viewerCommand.getUsername())) //if Username in DB id is EQUAL to Username in FORM id
+                        .findFirst().get().getId().equals(viewerCommand.getId())) {
+                    return true;
+                } else return false;
+            }else {
+                if (viewerSet.stream().filter(viewer -> viewer.getEmail().equals(viewerCommand.getEmail()))
+                        .findFirst().isPresent()) {
+                    if (viewerSet.stream().filter(viewer -> viewer.getEmail().equals(viewerCommand.getEmail()))
+                            .findFirst().get().getId().equals(viewerCommand.getId())) {
+                        return true;
+                    } else return false;
+                }else return true;
+            }
         }else {
             if(viewerSet.stream().filter(viewer -> viewer.getUsername().equals(viewerCommand.getUsername()))
                     .findFirst().isPresent()){
@@ -38,6 +55,5 @@ public class ViewerValidation implements ConstraintValidator<UniqueViewer, Viewe
                 }else return true;
             }
         }
-
     }
 }
